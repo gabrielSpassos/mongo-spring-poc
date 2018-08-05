cd3e68aec07d80ad564de10a67b8b779215e1312
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-08-05 17:57:11 -0300

    :wrench: ajustando configurações do banco

--- src/main/java/br/gabrielspassos/poc/mongopoc/entities/CardEntity.java
+++ src/main/java/br/gabrielspassos/poc/mongopoc/entities/CardEntity.java
@@ -1,7 +1,9 @@
 package br.gabrielspassos.poc.mongopoc.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "cards")
 public class CardEntity {
 
     @Id

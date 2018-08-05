b33af3de9a8253f7b906f5e15c070f30847f0aa3
gabrielSpassos <gabrielsantos45725@gmail.com>
2018-08-05 17:46:44 -0300

    :sparkles: adding routes de update e get by number

--- src/main/java/br/gabrielspassos/poc/mongopoc/controllers/CardController.java
+++ src/main/java/br/gabrielspassos/poc/mongopoc/controllers/CardController.java
@@ -48,6 +48,27 @@ public class CardController {
     }
 
     @ApiOperation(
+            value="Get card by number",
+            response=CardDto.class,
+            notes="This operation return a card")
+    @ApiResponses(value= {
+            @ApiResponse(
+                    code=200,
+                    message="Return one card",
+                    response=CardDto.class
+            )
+    })
+    @GetMapping(value = "/cards/{number}")
+    public ResponseEntity<?> getCardByNumber(@PathVariable("number") String number){
+        return Stream.of(number)
+                .map(num -> cardService.getCardByNumber(num))
+                .map(this::convertToDto)
+                .map(ResponseEntity::ok)
+                .findFirst()
+                .get();
+    }
+
+    @ApiOperation(
             value="Save card",
             response=CardDto.class,
             notes="This operation save a card")
@@ -66,6 +87,29 @@ public class CardController {
                 .map(this::convertToDto)
                 .map(ResponseEntity::ok)
                 .findAny()
+                .get();
+    }
+
+    @ApiOperation(
+            value="Update card",
+            response=CardDto.class,
+            notes="This operation update's a card")
+    @ApiResponses(value= {
+            @ApiResponse(
+                    code=200,
+                    message="Return the updated card",
+                    response=CardDto.class
+            )
+    })
+    @PutMapping(value = "/cards/{number}")
+    public ResponseEntity<?> updateCardByNumber(@PathVariable("number") String number,
+                                                @RequestBody SaveCardDto saveCardDto){
+        return Stream.of(saveCardDto)
+                .map(this::convertToEntity)
+                .map(entity -> cardService.updateCard(number, entity))
+                .map(this::convertToDto)
+                .map(ResponseEntity::ok)
+                .findFirst()
                 .get();
     }
 
--- src/main/java/br/gabrielspassos/poc/mongopoc/services/CardService.java
+++ src/main/java/br/gabrielspassos/poc/mongopoc/services/CardService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class CardService {
@@ -22,6 +23,14 @@ public class CardService {
         }
     }
 
+    public CardEntity getCardByNumber(String number) {
+        try{
+            return cardRepository.findByNumber(number);
+        }catch (Exception e) {
+            throw new IllegalStateException(String.format("Failed to get card number %s", number));
+        }
+    }
+
     public CardEntity saveCard(CardEntity cardEntity) {
         try {
             cardEntity.setNumber(createCardNumber());
@@ -31,7 +40,23 @@ public class CardService {
         }
     }
 
+    public CardEntity updateCard(String number, CardEntity cardEntity) {
+        try {
+            CardEntity card = getCardByNumber(number);
+            card = updateCardNameAndType(cardEntity.getName(), cardEntity.getType(), card);
+            return cardRepository.save(card);
+        } catch (Exception e) {
+            throw new IllegalStateException("Failed to update card");
+        }
+    }
+
     private String createCardNumber() {
         return RandomStringUtils.randomNumeric(5,5);
+    }
+
+    private CardEntity updateCardNameAndType(String name, String type, CardEntity card){
+        card.setName(name);
+        card.setType(type);
+        return card;
     }
 }
